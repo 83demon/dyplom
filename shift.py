5ebae521235f6de08615c29b19911a928f04e52b
vo1ku <vkuzko007@gmail.com>
2022-11-12 19:41:42 +0200

    Fixed bug of inheriting global variable in method

--- shift.py
+++ shift.py
@@ -9,11 +9,11 @@ shape = img.shape
 
 def shift(img,vertical_shift=0,horizontal_shift=0):
     if vertical_shift>=0 and horizontal_shift>=0:
-        return np.pad(img,((vertical_shift,0),(horizontal_shift,0),(0,0)))[:shape[0],:shape[1],:]
+        return np.pad(img,((vertical_shift,0),(horizontal_shift,0),(0,0)))[:img.shape[0],:img.shape[1],:]
     elif vertical_shift<0 and horizontal_shift>=0:
-        return np.pad(img,((0,-vertical_shift),(horizontal_shift,0),(0,0)))[-vertical_shift:,:shape[1],:]
+        return np.pad(img,((0,-vertical_shift),(horizontal_shift,0),(0,0)))[-vertical_shift:,:img.shape[1],:]
     elif vertical_shift>=0 and horizontal_shift<0:
-        return np.pad(img,((vertical_shift,0),(0,-horizontal_shift),(0,0)))[:shape[0],-horizontal_shift:,:]
+        return np.pad(img,((vertical_shift,0),(0,-horizontal_shift),(0,0)))[:img.shape[0],-horizontal_shift:,:]
     else:
         return np.pad(img,((0,-vertical_shift),(0,-horizontal_shift),(0,0)))[-vertical_shift:,-horizontal_shift:,:]
 
@@ -59,26 +59,26 @@ def column_measure(img,window_size=0):
 
     return measure_data_copy[window_size//2:-window_size//2]
 
+if __name__=="__main__":
+    window_size = 5
+    for i in range(3):
+        for j in range(3):
+            if i==j and not i:
+                continue
 
-window_size = 5
-for i in range(3):
-    for j in range(3):
-        if i==j and not i:
-            continue
+            folder_prefix = 'results\\'
+            img_name = f'vertical {i}; horizontal {j}'
+            histogram_prefix = 'hist '
+            res_prefix = 'measure of '
+            visualizatin_prefix = 'visualization of '
 
-        folder_prefix = 'results\\'
-        img_name = f'vertical {i}; horizontal {j}'
-        histogram_prefix = 'hist '
-        res_prefix = 'measure of '
-        visualizatin_prefix = 'visualization of '
-
-        img_sh = shift(img,i,j)
-        res = abs(img_sh-img)
-        measure_ij = measure(res,window_size)
-        print(f"Measure for {i}-{j}: {measure_ij}")
-        col_measure = column_measure(res,window_size)
-        make_hist(col_measure,f'Histogram of vertical {i} and horizontal {j}.\nMaximum value is {measure_ij}',('value of a column measure','number of columns'),folder_prefix+histogram_prefix+img_name)
-        visualize_residuals(col_measure,'',('number of  column','value of a column measure'),folder_prefix+res_prefix+img_name)
-        visualize(res,folder_prefix+img_name+'.jpg')
+            img_sh = shift(img,i,j)
+            res = abs(img_sh-img)
+            measure_ij = measure(res,window_size)
+            print(f"Measure for {i}-{j}: {measure_ij}")
+            col_measure = column_measure(res,window_size)
+            make_hist(col_measure,f'Histogram of vertical {i} and horizontal {j}.\nMaximum value is {measure_ij}',('value of a column measure','number of columns'),folder_prefix+histogram_prefix+img_name)
+            visualize_residuals(col_measure,'',('number of  column','value of a column measure'),folder_prefix+res_prefix+img_name)
+            visualize(res,folder_prefix+img_name+'.jpg')
 
 
