5f866ae900d1adecec5c4d09f9a0cae28886244c
vo1ku <vkuzko007@gmail.com>
2023-02-28 20:41:14 +0200

    Intermediate results in local_and_global maximum_method.py

--- local_and_global maximum_method.py
+++ local_and_global maximum_method.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 from utils import Image_Helper, SpecialList, Vector
 from point_finders import MaximumColorFinder
 
@@ -12,6 +14,8 @@ class LocalAndGlobalMaximumShift(Image_Helper):
         self.Images_main_split = self.Image_main.split(height_num=split_vert, width_num=split_horiz)
         self.Images_shifted_split = self.Image_shifted.split(height_num=split_vert, width_num=split_horiz)
 
+        self.vert_shift = vert_shift
+        self.horiz_shift = horiz_shift
         self.split_vert = split_vert
         self.split_horiz = split_horiz
 
@@ -21,7 +25,52 @@ class LocalAndGlobalMaximumShift(Image_Helper):
 
         self.channel = channel
 
-    def find_global_vector(self):
+        self.O = None
+        self.A = None
+        self.B = None
+        self.A1 = None
+        self.B1 = None
+        self.O_O1 = None # global shift, "s" vector
+        self.O_A1 = None
+        self.O_B1 = None
+
+    def main(self):
+        self.O_O1 = self._find_global_vector()
+        self._assign_points(self.O_O1)
+        OA = Vector(self.O,self.A)
+        OB = Vector(self.O,self.B)
+        self.O_A1 = Vector(self.A,self.A1) + self.O_O1 + OA
+        self.O_B1 = Vector(self.B,self.B1) + self.O_O1 + OB
+        a = (self.O_A1 - OA).get_direction()
+        b = (self.O_B1 -OB).get_direction()
+        transformation_matrix = np.matrix([a,b]).T
+        #inv_matrix = np.linalg.pinv(transformation_matrix)
+        #out = cv2.warpAffine(self.Image_main.img, inv_matrix, (self.Image_main.shape[1], self.Image_main.shape[0]))
+        #Image_Helper(img=out).show()
+
+
+    def _assign_points(self,global_vect):
+        self.O = global_vect.end.copy()
+        A_A1, B_B1 = self._select_A_and_B()
+        self.A = A_A1.start.copy()
+        self.A1 = A_A1.end.copy()
+        self.B = B_B1.start.copy()
+        self.B1 = B_B1.end.copy()
+
+    def _select_A_and_B(self):
+        assert len(self.vectors)>=2
+
+        if len(selected:=[v for v in self.vectors if v.get_direction()==SpecialList([self.vert_shift,self.horiz_shift])]) >= 2:
+            return selected[:2]
+        elif len(selected)==1:
+            item_first = selected.pop()
+            for item_second in self.vectors:
+                if item_second!=item_first:
+                    return [item_first,item_second]
+        else:
+            return self.vectors[:2]
+
+    def _find_global_vector(self):
         for i in range(self.split_vert):
             for j in range(self.split_horiz):
                 img, shifted_img = self.Images_main_split[j + i * self.split_vert].img, \
@@ -51,13 +100,15 @@ class LocalAndGlobalMaximumShift(Image_Helper):
         return Vector(self.global_avg,self.global_avg_shifted)
 
 
-method = LocalAndGlobalMaximumShift(filepath=r"photos\\mountains.jpg",split_vert=3,split_horiz=3,
+method = LocalAndGlobalMaximumShift(filepath=r"photos\\moon.jpg",split_vert=2,split_horiz=2,
                                     vert_shift=3,horiz_shift=7,channel=2)
 print(method.img.shape)
-global_vect = method.find_global_vector()
+#method.main()
+global_vect = method._find_global_vector()
 print("Vectors: ")
 for vector in method.vectors:
     print(vector)
-    vector.draw(method.Image_main.img)
+    vector.draw(method.Image_main.img,size=4,color=(0,255,255))
 print("Global: ",global_vect)
-global_vect.display(is_canvas=True,canvas=method.Image_main.img,name="Vectors",show_coordinates=True,size=3,color=(0,255,255))
+global_vect.display(is_canvas=True,canvas=method.Image_main.img,name="Vectors",show_coordinates=True,size=3,color=(255,2,255))
+method.Image_main.save("211test.jpg")
